867b7ef7dc74ef1c5ec865b338c032a9e02cb5b0
JSLandbo <jacob@landbo.dk>
2026-03-31 23:23:33 +0200

    water, lava, bugfix.

--- ToTheEndOfTheWorld/Gameplay/World/WorldBlockDefinitionResolver.cs
+++ ToTheEndOfTheWorld/Gameplay/World/WorldBlockDefinitionResolver.cs
@@ -1,68 +1,52 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using ModelLibrary.Concrete.Blocks;
+using ModelLibrary.Ids;
 using UtilityLibrary;
+using BlockDefinition = System.Collections.Generic.KeyValuePair<int, (string Name, Microsoft.Xna.Framework.Graphics.Texture2D Texture, ModelLibrary.Concrete.Blocks.Block block)>;
 
 namespace ToTheEndOfTheWorld.Gameplay.World
 {
     public sealed class WorldBlockDefinitionResolver
     {
-        private const int AirBlockId = 0;
-        private const int GrassBlockId = 1;
-        private const int DirtBlockId = 2;
-        private const int RockBlockId = 4;
-        private const int LavaBlockId = 38;
-
-        private readonly WorldElementsRepository blocks;
-        private readonly KeyValuePair<int, (string Name, Texture2D Texture, Block block)>[] overlayBlockDefinitions;
-        private readonly KeyValuePair<int, (string Name, Texture2D Texture, Block block)> airBlockDefinition;
-        private readonly KeyValuePair<int, (string Name, Texture2D Texture, Block block)> grassBlockDefinition;
-        private readonly KeyValuePair<int, (string Name, Texture2D Texture, Block block)> dirtBlockDefinition;
-        private readonly KeyValuePair<int, (string Name, Texture2D Texture, Block block)> rockBlockDefinition;
-        private readonly KeyValuePair<int, (string Name, Texture2D Texture, Block block)> lavaBlockDefinition;
+        private readonly Dictionary<int, BlockDefinition> definitions;
+        private readonly BlockDefinition[] overlayBlockDefinitions;
 
         public WorldBlockDefinitionResolver(WorldElementsRepository blocks)
         {
-            this.blocks = blocks;
-            airBlockDefinition = new KeyValuePair<int, (string Name, Texture2D Texture, Block block)>(AirBlockId, blocks[AirBlockId]);
-            grassBlockDefinition = new KeyValuePair<int, (string Name, Texture2D Texture, Block block)>(GrassBlockId, blocks[GrassBlockId]);
-            dirtBlockDefinition = new KeyValuePair<int, (string Name, Texture2D Texture, Block block)>(DirtBlockId, blocks[DirtBlockId]);
-            rockBlockDefinition = new KeyValuePair<int, (string Name, Texture2D Texture, Block block)>(RockBlockId, blocks[RockBlockId]);
-            lavaBlockDefinition = new KeyValuePair<int, (string Name, Texture2D Texture, Block block)>(LavaBlockId, blocks[LavaBlockId]);
+            definitions = [];
+            List<BlockDefinition> overlays = [];
 
-            List<KeyValuePair<int, (string Name, Texture2D Texture, Block block)>> overlayDefinitions = [];
-            int index = 0;
-            foreach (KeyValuePair<int, (string Name, Texture2D Texture, Block block)> block in blocks)
+            foreach (BlockDefinition block in blocks)
             {
+                definitions[block.Key] = block;
+
                 if (IsOverlayBlock(block.Key))
                 {
-                    overlayDefinitions.Add(block);
+                    overlays.Add(block);
                 }
-
-                index++;
             }
 
-            overlayBlockDefinitions = overlayDefinitions.ToArray();
-            Array.Sort(overlayBlockDefinitions, (left, right) => right.Key.CompareTo(left.Key));
+            overlayBlockDefinitions = [.. overlays];
+            Array.Sort(overlayBlockDefinitions, static (left, right) => right.Key.CompareTo(left.Key));
         }
 
-        public KeyValuePair<int, (string Name, Texture2D Texture, Block block)> GetWorldBlock(float x, float y)
+        public BlockDefinition GetWorldBlock(float x, float y)
         {
-            if (y <= airBlockDefinition.Value.block.Info.MaximumDepth)
+            BlockDefinition air = GetDefinition(GameIds.RuntimeBlocks.Air);
+            if (y <= air.Value.block.Info.MaximumDepth)
             {
-                return airBlockDefinition;
+                return air;
             }
 
-            if (y == grassBlockDefinition.Value.block.Info.MinimumDepth)
+            BlockDefinition grass = GetDefinition(GameIds.Blocks.Grass);
+            if (y == grass.Value.block.Info.MinimumDepth)
             {
-                return grassBlockDefinition;
+                return grass;
             }
 
-            KeyValuePair<int, (string Name, Texture2D Texture, Block block)> baseTerrain = ResolveBaseTerrain(x, y);
-
-            foreach (KeyValuePair<int, (string Name, Texture2D Texture, Block block)> block in overlayBlockDefinitions)
+            foreach (BlockDefinition block in overlayBlockDefinitions)
             {
                 if (MatchesDefinition(block, x, y))
                 {
@@ -70,49 +54,61 @@ namespace ToTheEndOfTheWorld.Gameplay.World
                 }
             }
 
-            return baseTerrain;
+            return ResolveBaseTerrain(x, y);
+        }
+
+        private BlockDefinition ResolveBaseTerrain(float x, float y)
+        {
+            float waterNoise = (float)SimplexNoise.Singleton.Noise01((x * 0.08f) + 173.0f, (y * 0.08f) + 421.0f) * 100.0f;
+            
+            BlockDefinition water = GetDefinition(GameIds.Blocks.Water);
+            if (IsWithinDepth(y, water) && waterNoise <= 5.0f)
+            {
+                return water;
+            }
+
+            float rockNoise = (float)SimplexNoise.Singleton.Noise01((x * 0.12f) + 91.0f, (y * 0.12f) + 37.0f) * 100.0f;
+            float lavaNoise = (float)SimplexNoise.Singleton.Noise01((x * 0.08f) + 241.0f, (y * 0.08f) + 503.0f) * 100.0f;
+
+            float rockChance = MathHelper.Clamp(6.0f + ((y - 12.0f) * 0.012f), 6.0f, 78.0f);
+            float lavaChance = y < 2000.0f ? 0.0f : MathHelper.Clamp((y - 2000.0f) * 0.004f, 0.0f, 18.0f);
+
+            BlockDefinition lava = GetDefinition(GameIds.Blocks.Lava);
+            if (IsWithinDepth(y, lava) && lavaNoise <= lavaChance)
+            {
+                return lava;
+            }
+
+            if (rockNoise <= rockChance)
+            {
+                return GetDefinition(GameIds.Blocks.Rock);
+            }
+
+            return GetDefinition(GameIds.Blocks.Dirt);
         }
 
         public bool IsObstructed(ModelWorld world, Vector2 worldPosition)
         {
             Block block = GetWorldBlock(worldPosition.X, worldPosition.Y).Value.block;
-
-            if (block.Ethereal || world.WorldTrails.ContainsKey(worldPosition))
-            {
-                return false;
-            }
-
-            return true;
+            return !block.Ethereal && !world.WorldTrails.ContainsKey(worldPosition);
         }
 
-        private KeyValuePair<int, (string Name, Texture2D Texture, Block block)> ResolveBaseTerrain(float x, float y)
+
+        private BlockDefinition GetDefinition(int blockId)
         {
-            float rockNoise = (float)SimplexNoise.Singleton.Noise01((x * 0.12f) + 91.0f, (y * 0.12f) + 37.0f) * 100.0f;
-            float lavaNoise = (float)SimplexNoise.Singleton.Noise01((x * 0.08f) + 241.0f, (y * 0.08f) + 503.0f) * 100.0f;
-
-            float rockChance = MathHelper.Clamp(6.0f + ((y - 12.0f) * 0.012f), 6.0f, 78.0f);
-            float lavaChance = y < 2000.0f
-                ? 0.0f
-                : MathHelper.Clamp((y - 2000.0f) * 0.004f, 0.0f, 18.0f);
-
-            if (y >= lavaBlockDefinition.Value.block.Info.MinimumDepth && lavaNoise <= lavaChance)
-            {
-                return lavaBlockDefinition;
-            }
-
-            if (rockNoise <= rockChance)
-            {
-                return rockBlockDefinition;
-            }
-
-            return dirtBlockDefinition;
+            return definitions[blockId];
         }
 
-        private static bool MatchesDefinition(KeyValuePair<int, (string Name, Texture2D Texture, Block block)> block, float x, float y)
+        private static bool IsWithinDepth(float y, BlockDefinition block)
+        {
+            return y >= block.Value.block.Info.MinimumDepth && y <= block.Value.block.Info.MaximumDepth;
+        }
+
+        private static bool MatchesDefinition(BlockDefinition block, float x, float y)
         {
             ModelLibrary.Abstract.Blocks.ABlockInfo info = block.Value.block.Info;
 
-            if (y > info.MaximumDepth || y < info.MinimumDepth)
+            if (y < info.MinimumDepth || y > info.MaximumDepth)
             {
                 return false;
             }
@@ -123,13 +119,14 @@ namespace ToTheEndOfTheWorld.Gameplay.World
 
         private static bool IsOverlayBlock(int blockId)
         {
-            return blockId != -2
-                && blockId != -1
-                && blockId != AirBlockId
-                && blockId != GrassBlockId
-                && blockId != DirtBlockId
-                && blockId != RockBlockId
-                && blockId != LavaBlockId;
+            return blockId != GameIds.RuntimeBlocks.Breaking
+                && blockId != GameIds.RuntimeBlocks.Background
+                && blockId != GameIds.RuntimeBlocks.Air
+                && blockId != GameIds.Blocks.Water
+                && blockId != GameIds.Blocks.Grass
+                && blockId != GameIds.Blocks.Dirt
+                && blockId != GameIds.Blocks.Rock
+                && blockId != GameIds.Blocks.Lava;
         }
     }
 }
